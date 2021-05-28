59b2a02f40c44b1492767d2d3d84d072597da0a4
jmasonlee <jmasonlee@gmail.com>
2021-05-28 06:51:36 -0600

    t Create test blank for hotkeyProcessor

---
+++ src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessorTest.java
@@ -0,0 +1,20 @@
+package org.teachingkidsprogramming.typingdeepdive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotkeyProcessorTest {
+
+    @Test
+    void keyTypedDoesNothing() {
+    }
+
+    @Test
+    void keyPressedDetectsKey() {
+    }
+
+    @Test
+    void keyReleasedDoesNothing() {
+    }
+}
