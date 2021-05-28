2aaf42d43b56aea986e05a870b9c6a00f25003bf
jmasonlee <jmasonlee@gmail.com>
2021-05-28 07:03:54 -0600

    f  HotkeyProcessor can interpret key events

--- src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessor.java
+++ src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessor.java
@@ -1,6 +1,7 @@
 package org.teachingkidsprogramming.typingdeepdive;
 
 import java.awt.event.KeyEvent;
+import java.util.function.Consumer;
 
 public class HotkeyProcessor extends KeyProcessor {
     @Override
@@ -10,7 +11,11 @@ public class HotkeyProcessor extends KeyProcessor {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        //do nothing
+
+    }
+
+    public void keyPressed(KeyEvent e, Consumer<Character> processLetter) {
+        processLetter.accept(KeyEvent.getKeyText(e.getKeyCode()).charAt(0));
     }
 
     @Override
--- src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessorTest.java
+++ src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessorTest.java
@@ -2,19 +2,22 @@ package org.teachingkidsprogramming.typingdeepdive;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotkeyProcessorTest {
 
     @Test
-    void keyTypedDoesNothing() {
-    }
-
-    @Test
     void keyPressedDetectsKey() {
-    }
+        DeepDiveTypingGame game = new DeepDiveTypingGame();
+        KeyEvent keyEvent = new KeyEvent(new Button("b"), 1, 20, 1, KeyEvent.VK_A, 'A');
+        HotkeyProcessor hotkeyProcessor = new HotkeyProcessor();
+        final Character[] letter = {Character.MIN_VALUE};
 
-    @Test
-    void keyReleasedDoesNothing() {
+        hotkeyProcessor.keyPressed(keyEvent, l -> letter[0] = l);
+
+        assertEquals(letter[0], keyEvent.getKeyChar());
     }
 }
