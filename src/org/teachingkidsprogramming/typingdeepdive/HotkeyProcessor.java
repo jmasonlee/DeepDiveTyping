dc9f1ae351a886018457f5df482e49b1f360c45a
jmasonlee <jmasonlee@gmail.com>
2021-05-28 05:58:29 -0600

    b  Commit Hotkey processor

---
+++ src/org/teachingkidsprogramming/typingdeepdive/HotkeyProcessor.java
@@ -0,0 +1,20 @@
+package org.teachingkidsprogramming.typingdeepdive;
+
+import java.awt.event.KeyEvent;
+
+public class HotkeyProcessor extends KeyProcessor {
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //do nothing
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //do nothing
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //do nothing
+    }
+}
